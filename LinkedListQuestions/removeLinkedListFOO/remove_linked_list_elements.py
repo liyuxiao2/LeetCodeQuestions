7df4a963dbe569dbdf974b4b73b3e003eccf5b5a
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2024-06-25 13:07:26 -0400

    restored files

---
+++ LinkedListQuestions/removeLinkedListFOO/remove_linked_list_elements.py
@@ -0,0 +1,35 @@
+from list_node import ListNode
+class Solution:
+    def remove_nodes_with_count(self, head, target_counts):
+        """
+        Removes nodes from the linked list based on target values and desired removal counts.
+
+        Args:
+            head: The head node of the linked list.
+            target_counts: A list of tuples where each tuple represents a target value and its desired removal count.
+
+        Returns:
+            The modified head node of the linked list.
+        """
+        
+        #creates a dictionary storing key, value pairs
+        target_dict = dict(target_counts)
+        
+        # Dummy node to handle edge cases easily
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        cur = head
+
+        while cur != None:
+            if cur.val in target_dict:
+                if target_dict[cur.val] > 0:
+                    target_dict[cur.val] -= 1
+                    prev.next = cur.next  # Remove the node
+                else:
+                    prev = cur  # Move to the next node
+            else:
+                prev = cur  # Move to the next node
+            cur = cur.next
+
+        return dummy.next
