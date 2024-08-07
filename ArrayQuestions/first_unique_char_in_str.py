54e5e3142fe9028627196902feca10cfb6f188f5
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2024-08-07 09:57:42 -0400

    solved first_unique char in string
    used the fact that dictionaries remain unsorted to solve.
    
     It made it easier to check when the unique character appeared in the string since the order of the dictionary reflected the order in which I added the values in.

---
+++ ArrayQuestions/first_unique_char_in_str.py
@@ -0,0 +1,50 @@
+'''
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+'''
+
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        
+        #keeps track of instances of the same character
+        char_count = {}
+        
+        for i in range(len(s)):
+            if s[i] in char_count:
+                char_count[s[i]] += 1
+            else:
+                char_count[s[i]] = 1
+                
+        #loop through s, when we see the first instance in which s[i] has a count of  1, return that index
+        for i in range(len(s)):
+            if(char_count[s[i]] == 1):
+                return i
+        return -1
+    
+
