66b822aaaf61b7dc1c7ffa1cacf27a47fd31bab9
liyuxiao2 <liyuxiao2@gmail.com>
2025-05-23 12:46:29 -0400

    finished backtrace string

--- Stack/back_space_str_compare.py
+++ Stack/back_space_str_compare.py
@@ -1,3 +1,39 @@
+'''
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+ 
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+Example 2:
+
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+Example 3:
+
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 200
+s and t only contain lowercase letters and '#' characters.
+ 
+
+Follow up: Can you solve it in O(n) time and O(1) space?
+
+
+'''
+
+
 class Solution(object):
     def backspaceCompare(self, s, t):
         """
@@ -7,18 +43,21 @@ class Solution(object):
         """
         s_string = ""
         t_string = ""
-
+        
         string_stack = []
 
         for i in s:
             if i == "#":
+                #check if the stack is empty
                 if(string_stack):
                     string_stack.pop()
             else:
                 string_stack.append(i)
         
+        #convert the list into a string
         s_string = "".join(string_stack)
-
+        
+        #clear
         string_stack = []
 
         for i in t:
@@ -29,5 +68,6 @@ class Solution(object):
                 string_stack.append(i)
         
         t_string = "".join()
-
+        
+        
         return (t_string==s_string)
